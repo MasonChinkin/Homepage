8cd1657e63e5f79c0f8389397e52cc75d6024421
Mason Chinkin <mason.chinkin@gmail.com>
2023-10-08 16:32:38 -0700

    stuff

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -2,9 +2,54 @@ import path from 'path'
 import { Configuration } from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { getExternals, getTemplateParameters } from './externalizedLibs'
-// import { GenerateSW } from 'workbox-webpack-plugin'
+import { GenerateSW } from 'workbox-webpack-plugin'
 
 const isProduction = process.env.NODE_ENV === 'production'
+
+const swPlugin = isProduction
+  ? [
+      new GenerateSW({
+        maximumFileSizeToCacheInBytes: 5000000,
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              const fonts = [
+                'https://use.fontawesome.com/releases/v5.7.0/css/all.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://fonts.googleapis.com/css?family=Roboto',
+              ]
+
+              if (fonts.includes(url.toString())) {
+                return true
+              }
+
+              if (url.toString().includes('bundle.js?')) {
+                return true
+              }
+
+              if (['.webp', 'png', 'jpg'].some(url.toString().includes)) {
+                return true
+              }
+
+              return false
+            },
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'StaleWhileRevalidate_Cache',
+              expiration: {
+                maxAgeSeconds: 24 * 60 * 60 * 60,
+                purgeOnQuotaError: true,
+              },
+            },
+          },
+        ],
+        skipWaiting: true,
+        clientsClaim: true,
+        cleanupOutdatedCaches: true,
+      }),
+    ]
+  : []
 
 const config: Configuration = {
   mode: 'production',
@@ -41,42 +86,7 @@ const config: Configuration = {
       hash: true,
       templateParameters: getTemplateParameters(isProduction),
     }),
-    // new GenerateSW({
-    //   maximumFileSizeToCacheInBytes: 5000000,
-    //   runtimeCaching: [
-    //     {
-    //       urlPattern: ({ url }) => {
-    //         const fonts = [
-    //           'https://use.fontawesome.com/releases/v5.7.0/css/all.css',
-    //           'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-    //           'https://fonts.googleapis.com/icon?family=Material+Icons',
-    //           'https://fonts.googleapis.com/css?family=Roboto',
-    //         ]
-
-    //         if (fonts.includes(url.toString())) {
-    //           return true
-    //         }
-
-    //         if (url.toString().includes('bundle.js?')) {
-    //           return true
-    //         }
-
-    //         return false
-    //       },
-    //       handler: 'CacheFirst',
-    //       options: {
-    //         cacheName: 'CacheFirst_Cache',
-    //         expiration: {
-    //           maxAgeSeconds: 24 * 60 * 60 * 60,
-    //           purgeOnQuotaError: true,
-    //         },
-    //       },
-    //     },
-    //   ],
-    //   skipWaiting: true,
-    //   clientsClaim: true,
-    //   cleanupOutdatedCaches: true,
-    // }),
+    ...swPlugin,
   ],
 }
 
