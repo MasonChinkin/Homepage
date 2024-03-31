ddfd889719e5032044215bd227d6020350da4cc2
Mason Chinkin <mason.chinkin@gmail.com>
2024-03-31 12:08:17 -0700

    fix test

--- src/components/Profile.test.tsx
+++ src/components/Profile.test.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import Profile from './Profile'
+import { Component as Profile } from './Profile'
 
 const component = (
   <BrowserRouter>
