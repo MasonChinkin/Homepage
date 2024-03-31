581500e5b1bca81425782705429819f0fe45ddd8
Mason Chinkin <mason.chinkin@gmail.com>
2024-03-31 12:46:26 -0700

    test function

--- functions/api/test-fetch.ts
+++ functions/api/test-fetch.ts
@@ -1,5 +1,7 @@
 export const onRequest: PagesFunction = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
 
-  return res.json()
+  const data = await res.json()
+
+  return new Response(JSON.stringify(data))
 }
